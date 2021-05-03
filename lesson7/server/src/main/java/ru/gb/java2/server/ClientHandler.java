cddd9cd9ceeedf47233d5501c35ee2beccb50905
obelix <sonicx921@yandex.ru>
2021-05-03 19:14:09 +0300

    Задание 7 в том числе со звездочкой.

---
+++ lesson7/server/src/main/java/ru/gb/java2/server/ClientHandler.java
@@ -0,0 +1,107 @@
+package ru.gb.java2.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Socket socket;
+    private Server server;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String username;
+
+    public ClientHandler(Socket socket, Server server) throws IOException {
+        this.socket = socket;
+        this.in = new DataInputStream(socket.getInputStream());
+        this.out = new DataOutputStream(socket.getOutputStream());
+        this.server = server;
+
+        new Thread(() -> {
+            try {
+                //цикл авторизации
+                while (true) {
+                    String msg = in.readUTF();
+                    // /login Alex
+                    if(msg.startsWith("/login ")) {
+                        String usernameFromLogin = msg.split("\\s")[1];
+
+                        if(server.isNickBusy(usernameFromLogin)) {
+                            sendMessage("/login_failed Current nickname has already been occupied");
+                            continue;
+                        }
+
+                        username = usernameFromLogin;
+                        sendMessage("/login_ok " + username);
+                        server.subscribe(this);
+                        break;
+                    }
+                }
+
+                //цикл общения
+                while (true) {
+                    String msg = in.readUTF();
+
+                    if(msg.startsWith("/who_am_i")) {
+                        this.sendMessage("Ваш логин "+ username + "\n");
+                        continue;
+                    }
+                    if(msg.startsWith("/w ")) {
+                        //вот тут ощибки могут быть, т.к. строка не обязатнльно состоит из 3х частей
+                        String[] parts = msg.split("\\s", 3);
+                        server.privateMessage(this, parts[2], parts[1]);
+                        continue;
+                    }
+                    if(msg.startsWith("/exit")) {
+                        //отправляем сообщение клиенту о выходе
+                        //метод чайника и гвоздя
+                        server.commandMessage(this,"/exit", this.username);
+                        server.broadcastMessage(this.getUsername() + " вышел из чата...");
+                        this.disconnect();
+                        break;
+                    }
+
+                    server.broadcastMessage(username + ": " + msg);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                disconnect();
+            }
+        }).start();
+    }
+
+    private void disconnect() {
+        server.unsubscribe(this);
+        if(in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(out != null) {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(String message) throws IOException {
+        out.writeUTF(message);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ lesson7/server/src/main/java/ru/gb/java2/server/Server.java
@@ -0,0 +1,71 @@
+package ru.gb.java2.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private int port;
+    private List<ClientHandler> clients;
+
+    public Server(int port) {
+        this.port = port;
+        this.clients = new ArrayList<>();
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен на порту " + port + "...");
+            while (true) {
+                System.out.println("Ждём нового клиента...");
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(socket, this);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+    public void broadcastMessage(String message) throws IOException {
+        for (ClientHandler clientHandler : clients) {
+            clientHandler.sendMessage(message);
+        }
+    }
+
+    public void commandMessage(ClientHandler sender, String message, String nick) throws IOException {
+        for (ClientHandler client : clients) {
+            if (client.getUsername().equals(nick)) {
+                client.sendMessage(message);
+                return;
+            }
+        }
+        sender.sendMessage("Такого пользователя нет!");
+    }
+
+    public void privateMessage(ClientHandler sender, String message, String nick) throws IOException {
+        for (ClientHandler client : clients) {
+            if (client.getUsername().equals(nick)) {
+                client.sendMessage("Личное сообщение от " + sender.getUsername() + ": " + message);
+                return;
+            }
+        }
+        sender.sendMessage("Такого пользователя нет!");
+    }
+
+    public boolean isNickBusy(String nickname) {
+        for (ClientHandler clientHandler : clients) {
+            if(clientHandler.getUsername().equals(nickname)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
